4737166dfce71009d48a9d2eef34d9c414888690
okuruu <Phantom_Ghost@outlook.co.id>
2023-10-25 08:03:26 +0700

    Change to production

--- src/lib/baseConfig.ts
+++ src/lib/baseConfig.ts
@@ -1,4 +1,4 @@
-const ENV:string = 'development';
+const ENV:string = 'production';
 
 export const baseConfiguration = {
     defaultURL : ENV == 'development' ? 'http://localhost:8080/api/Kosada/' : 'https://esdelfron.deabakery.co.id/api/Kosada/'
